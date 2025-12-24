ca986966b99b679a1fc1cac353804b71817f85eb
chamchi63 <tiger4399@naver.com>
2025-12-24 21:31:38 +0900

    middleware 규칙 넣어봄

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from fastapi import FastAPI
 import models
 from database import engine
 from api import auth, sheets # 방금 만든 auth 라우터
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Perpit API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 테스트 단계에서는 전체 허용, 나중에 프론트 주소만 넣기
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Express의 app.use('/auth', authRouter)와 동일
 app.include_router(auth.router)
